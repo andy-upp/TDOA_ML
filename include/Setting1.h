212e32a8f7664b457a52e59bf692a09a5bc61678
Alalakii <hw622@cornell.edu>
2018-10-21 14:00:33 -0400

    Modify datatype from float to double for more precision

--- include/Setting1.h
+++ include/Setting1.h
@@ -10,21 +10,22 @@ using namespace std;
 class Setting1 {
 public:
 	vector<MyPoint2D> rcvrs; //receivers
-	vector<float> timediffs;
+	vector<double> timediffs;
 	MyPoint2D t; // transmiter
 	MyPoint2D p; // iteration point
 	MyPoint2D real_tgt; // real position of target
-	float c; // signal speed
+	double c; // signal speed
 	int iter_limit; // iteration number limit
-	float norm_limit; // judge when to stop the iteration
+	double norm_limit; // judge when to stop the iteration
 
-	Setting1(const MyPoint2D&, const float&);
-	void addReceiver(MyPoint2D&, float&);
-	void setTransmiter(MyPoint2D*);
-	void setInitPoint(MyPoint2D*);
+	Setting1(const MyPoint2D&, const double&);
+	void addReceiver(MyPoint2D&, double&);
+	void setTransmiter(const MyPoint2D&);
+	void setInitPoint(const MyPoint2D&);
 	void setIterLimit(const int&);
-	void setNormLimit(const float&);
-	float timeCompute(MyPoint2D&);
+	void setNormLimit(const double&);
+	double timeCompute(MyPoint2D&);
+	double timeCompute(MyPoint2D&, const double&);
 	MyPoint2D localize();
 };
 
