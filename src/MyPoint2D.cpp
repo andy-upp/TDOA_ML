cad18380d4db4e96451e10a7366ebd407aa93b95
Alalakii <hw622@cornell.edu>
2018-10-21 14:01:00 -0400

    Modify datatype from float to double for more precision

--- src/MyPoint2D.cpp
+++ src/MyPoint2D.cpp
@@ -1,4 +1,6 @@
 #include <cmath>
+#include <random>
+#include <ctime>
 #include "../include/MyPoint2D.h"
 
 MyPoint2D::MyPoint2D() {
@@ -6,7 +8,7 @@ MyPoint2D::MyPoint2D() {
 	y = 0.f;
 }
 
-MyPoint2D::MyPoint2D(float _x, float _y) {
+MyPoint2D::MyPoint2D(double _x, double _y) {
 	x = _x;
 	y = _y;
 }
@@ -25,12 +27,29 @@ MyPoint2D MyPoint2D::operator -(const MyPoint2D& A) {
 	return C;
 }
 
-float MyPoint2D::norm() {
+double MyPoint2D::norm() {
     return sqrt(x*x + y*y);
 }
 
-float MyPoint2D::dist_square(MyPoint2D point) {
-	float diff_x = x - point.x;
-	float diff_y = y - point.y;
+double MyPoint2D::dist_square(MyPoint2D point) {
+	double diff_x = x - point.x;
+	double diff_y = y - point.y;
     return diff_x*diff_x + diff_y*diff_y;
+}
+
+void MyPoint2D::randomGenerate(const double& lower, const double& upper) {
+	double fractor1 = ((double) rand())/(double) RAND_MAX;
+	double fractor2 = ((double) rand())/(double) RAND_MAX;
+	double dist = upper - lower;
+	x = lower + dist*fractor1;
+	y = lower + dist*fractor2;
+}
+
+void MyPoint2D::randomGenerate(const double& x_lo, const double& x_up, const double& y_lo, const double& y_up) {
+	double fractor1 = ((double) rand())/(double) RAND_MAX;
+	double fractor2 = ((double) rand())/(double) RAND_MAX;
+	double dist_x = x_up - x_lo;
+	double dist_y = y_up - y_lo;
+	x = x_lo + dist_x*fractor1;
+	y = y_lo + dist_y*fractor2;
 }
